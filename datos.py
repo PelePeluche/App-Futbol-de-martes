d64b75bc5df4883575e0b561d7219484cb53842a
PelePeluche <fernandopffarias@gmail.com>
2022-03-30 18:17:57 -0300

    datos added

---
+++ datos.py
@@ -0,0 +1,189 @@
+datos = {
+    "Partido 1": {
+        "Equipo 1": {
+            "jugadores": {
+                "Yayeta",
+                "GonZi",
+                "Fede",
+                "Pera",
+                "Pancho",
+                "Gonza",
+                "Cabeza",
+            },
+            "goles": 2,
+        },
+        "Equipo 2": {
+            "jugadores": {
+                "Negro",
+                "Jandro",
+                "Ciego",
+                "Leo",
+                "Pini",
+                "Pelado",
+                "Pepe",
+            },
+            "goles": 5,
+        },
+    },
+    "Partido 2": {
+        "Equipo 1": {
+            "jugadores": {
+                "Bigornia",
+                "Gonza",
+                "GonZi",
+                "Pelado",
+                "Pini",
+                "Ciego",
+                "Joaco",
+            },
+            "goles": 10,
+        },
+        "Equipo 2": {
+            "jugadores": {"Cabeza", "Yayeta", "Nico", "Leo", "Pera", "Gato", "Monti"},
+            "goles": 7,
+        },
+    },
+    "Partido 3": {
+        "Equipo 1": {
+            "jugadores": {"Tavo", "Cabeza", "Leo", "Ciego", "Fede", "GonZi", "Pera"},
+            "goles": 7,
+        },
+        "Equipo 2": {
+            "jugadores": {
+                "Monti",
+                "Gonza",
+                "Pini",
+                "Pelado",
+                "Pepe",
+                "Negro",
+                "Yayeta",
+            },
+            "goles": 7,
+        },
+    },
+    "Partido 4": {
+        "Equipo 1": {
+            "jugadores": {"Pera", "Santiago", "Tomas", "Leo", "Monti", "Negro", "Pepe"},
+            "goles": 2,
+        },
+        "Equipo 2": {
+            "jugadores": {
+                "Gonza",
+                "Pelado",
+                "Pini",
+                "GonZi",
+                "Fede",
+                "Bigornia",
+                "Pancho",
+            },
+            "goles": 6,
+        },
+    },
+    "Partido 5": {
+        "Equipo 1": {
+            "jugadores": {"Cabeza", "Yayeta", "Pini", "Leo", "Monti", "Nico", "Negro"},
+            "goles": 4,
+        },
+        "Equipo 2": {
+            "jugadores": {
+                "Bigornia",
+                "Gonza",
+                "Ciego",
+                "Fede",
+                "GonZi",
+                "Pancho",
+                "Pelado",
+            },
+            "goles": 7,
+        },
+    },
+    "Partido 6": {
+        "Equipo 1": {
+            "jugadores": {"Nico", "Yayeta", "Pepe", "Pancho", "Pini", "Negro", "GonZi"},
+            "goles": 4,
+        },
+        "Equipo 2": {
+            "jugadores": {
+                "Bigornia",
+                "Pelado",
+                "Fede",
+                "Gonza",
+                "Cabeza",
+                "Pera",
+                "Kechi",
+            },
+            "goles": 5,
+        },
+    },
+}
+
+"""
+
+Nico-yayu-pepe-pancho-pini-negro-gonzi
+4
+Vs
+5
+Lb-pele-fede-gon-kb-pera-kechi"""
+
+"""
+Equipo 1: yayu(c) Gonzi Fede pera pancho gon KB
+2
+vs
+5
+Equipo 2: Indio(c) Jandro ciego leo pini pele pp
+
+
+Equipo 1 
+Lb
+Gon
+gonzi
+Pelé
+Pini
+Ciego
+Esbi
+10
+vs
+7
+Equipo 2
+kb
+yayu
+Nicolai
+Leo
+Pera
+Gato
+monti
+
+
+Tavo, kb, leo, ciego, Fede,
+Gonzi, pera
+7
+Vs
+7
+Monti, Gon, pini, pele, Pepe, indio, yayu
+
+
+Pera, Santi, Tomi, Leo, Monti,indio,pepe
+2 
+vs
+6
+gon,pele,pini,gonzi,fede,lb,pancho
+
+Kb
+Yayu
+Pini
+Leo
+Monti
+Nicolai
+Indio
+4
+-Vs-
+7
+Lb
+Gon
+Ciego
+Fede
+Gonzi
+Pancho
+Peluche
+
+"""
