13c6a9750651734c5938ded809edd85a1112490a
PelePeluche <fernandopffarias@gmail.com>
2022-03-30 17:36:04 -0300

    models created

--- models.py
+++ models.py
@@ -1,23 +1,27 @@
 import pony.orm as pony
 from datetime import date
+from enum import IntEnum
 from mixins import *
 
 
 db = pony.Database()
 
+# Definición de clase Jugador y sus atributos
 
-class Jugador(db.Entity):
+
+class Jugador(db.Entity, JugadorMixin):
     id_jugador = pony.PrimaryKey(int, auto=True)
     nombre = pony.Required(str)
-    jugados = pony.Optional(int)
-    puntos = pony.Optional(int)
     resultados = pony.Set("Resultado")
     partidos = pony.Set("Partido")
     equipos = pony.Set("Equipo", reverse="jugadores")
     capitan_de = pony.Set("Equipo", reverse="capitan")
 
 
-class Equipo(db.Entity):
+# Definición de clase Equipo y sus atributos
+
+
+class Equipo(db.Entity, EquipoMixin):
     id_equipo = pony.PrimaryKey(int, auto=True)
     partido = pony.Required("Partido")
     resultado = pony.Optional("Resultado")
@@ -27,7 +31,10 @@ class Equipo(db.Entity):
     pechera = pony.Optional(str)
 
 
-class Partido(db.Entity):
+# Definición de clase Partido y sus atributos
+
+
+class Partido(db.Entity, PartidoMixin):
     id_partido = pony.PrimaryKey(int, auto=True)
     cancha = pony.Optional("Cancha")
     jugadores_anotados = pony.Set(Jugador)
@@ -35,7 +42,10 @@ class Partido(db.Entity):
     fecha = pony.Optional(date)
 
 
-class Cancha(db.Entity):
+# Definición de clase Cancha y sus atributos
+
+
+class Cancha(db.Entity, CanchaMixin):
     id_cancha = pony.PrimaryKey(int, auto=True)
     nombre = pony.Optional(str)
     tamanio = pony.Optional(int)
@@ -43,15 +53,27 @@ class Cancha(db.Entity):
     partidos = pony.Set(Partido)
 
 
-class Resultado(db.Entity):
+# Definición de TAD Result
+
+
+class Result(IntEnum):
+    GANADO = 1
+    EMPATADO = 2
+    PERDIDO = 3
+
+
+# Definición de clase Resultado y sus atributos
+
+
+class Resultado(db.Entity, ResultadoMixin):
     id_resultado = pony.PrimaryKey(int, auto=True)
-    resultado = pony.Optional(str, unique=True)
+    resultado = pony.Required(Result, unique=True)
     equipos = pony.Set(Equipo)
     jugadores = pony.Set(Jugador)
 
 
 # Line used for debugging
-pony.pony.Set_sql_debug(True)
+pony.set_sql_debug(True)
 
 # Creation of tables for the models
 db.bind("sqlite", "database.sqlite", create_db=True)
